6bca33084729793fdba87629998fcd7d2a171957
CODERELY07 <164634420+CODERELY07@users.noreply.github.com>
2024-06-17 14:49:51 +0800

    search-trainee

--- index.php
+++ index.php
@@ -9,13 +9,47 @@ include('config/dbcheck.php');
 </head>
 <body>
     <div class="wrapper">
-        <form action="form.php" method="post">
-
-        <input type="text" name="name" >
-        <button type="submit" name="search">Search</button>
-        <input type="button" name="Add" value="New Applicant">
-        </form>
+        <div id='search-bar'>
+            <input type="text" name="search" id="search" autocomplete="off">
+            <button type="submit" id="search-btn" name="search-btn">Search</button>
+            <input type="button" id="Add" value="New Applicant">
+        </div>
     </div>
-    
+    <div id="table-data">
+
+    </div>
+    <!-- jQuery library -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script>
+        document.getElementById('Add').onclick = ()=>{
+            window.location = "form.php";
+        }
+
+        $(document).ready(function(){
+            $('#search-btn').on("click", function(){
+                var search_term = $('#search').val();
+                searching(search_term);
+            })
+
+            $('#search').on("keypress" , function (e){
+                if(e.which === 13){
+                    var search_term = $('#search').val();
+                    searching(search_term);
+                }
+            })
+            function searching(search_text){
+                console.log(search_text)
+                $.ajax({
+                    url: "search.php",
+                    type: "POST",
+                    data: {search: search_text},
+                    success: function (data){
+                        $('#table-data').html(data);
+                    }
+                })
+            }
+        });
+       
+    </script>
 </body>
 </html>
---
+++ search.php
@@ -0,0 +1,33 @@
+<?php
+    include('config/dbcheck.php');
+
+    if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['search'])){
+        $search_term = $_POST['search'];
+        
+        $sql = "SELECT * FROM trainees WHERE firstname LIKE '%$search_term%' OR middlename LIKE '%$search_term%' OR lastname LIKE '%$search_term%'";
+        $result = $connect->query($sql) or die("QUERY FAILED" . $connect->error);
+
+        $output = "";
+
+        if($result->num_rows > 0){
+            $output = "<table>
+            <tr>
+                <td>ID</td>
+                <td>First Name</td>
+            </tr>
+            ";
+            while($row = $result->fetch_assoc()){
+                $output .= "<tr>
+                    <td>{$row['id']}</td>
+                    <td>{$row['firstname']}</td>
+                </tr>";
+            }
+        }
+        $output .= "</table>";
+        $connect->close();
+
+        echo $output;
+    }else{
+        echo "<h2>No Record Found</h2>";
+    }
+?>
